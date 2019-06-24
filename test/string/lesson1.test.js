f10c1b56bb966befd267d16ead5bef97bdaba513
changxianglin <281532612@qq.com>
2019-06-25 00:57:18 +0800

    start lensson1

---
+++ test/string/lesson1.test.js
@@ -0,0 +1,5 @@
+import revertByWord from '../../code/string/lesson1'
+
+test('revertByWord:Let\'s take LeetCode contest', () => {
+  expect(revertByWord("Let's take LeetCode contest")).toBe('s\'teL ekat edoCteeL tsetnoc')
+})
