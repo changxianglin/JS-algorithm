8b0c6914fce7a523724d73d15e29538aa96966ee
changxianglin <281532612@qq.com>
2019-06-25 10:43:01 +0800

    start learn array

---
+++ code/array/lesson1.js
@@ -0,0 +1,26 @@
+export default (str) => {
+  let map = ['', 1, 'abc', 'def', 'ghi', 'jkl', 'mno', 'pqrs', 'tuv', 'wxyz']
+  let num = str.split('')
+  let code = []
+  num.forEach(item => {
+    if (map[item]) {
+      code.push(map[item])
+    }
+  })
+  let comb = (arr) => {
+    let tmp = []
+    for (let i = 0, il = arr[0].length; i < il; i++) {
+      for (let j = 0, jl = arr[1].length; j < jl; j++) {
+        tmp.push(`${arr[0][i]}${arr[1][j]}`)
+      }
+    }
+    arr.splice(0, 2, tmp)
+    if (arr.length > 1) {
+      comb(arr)
+    } else {
+      return tmp
+    }
+    return arr[0]
+  }
+  return comb(code)
+}
